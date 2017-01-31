b7cf9d9857148cf9e55c78477638b45801a9b71e
tinsleyfok <tslhuo@ucdavis.edu>
2017-01-31 13:16:38 -0800

    xxx

--- studio2/script.js
+++ studio2/script.js
@@ -21,7 +21,7 @@ console.log('reading');
     var result=document.getElementById('result');
       
        if (name == "" || time == "" || creature == "" || smell == "" || food == "") {
-            alert("Fill Out the Form, baby");
+            alert("Fill Out the Form, Baby");
         } else {
     //store userColor in a variable called userColor
 result.innerHTML=('As usual, you were waked up by the clock at ' + time +'.But unusual that '+ name+ ' is right next to you on your bed, eating ' + food +'.'+ name +"'s mouth smells like " + smell + ' and holy!'+ name+ ' tries to kiss you. You run away immediately but you cannot find the door out. Suddenly many '+ creature + ' are climbing down from the ceiling. And the room becomes smaller and smaller.<br><br><br><br> This must be a nightmare!<br><br><br><br>This must be a nightmare!<br><br><br><br><br><br> Yes. This is a nightmare. It is three hours after '+time+ ' now. You have already miss all day class.' );
