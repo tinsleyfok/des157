ea6c97de347d43be925a2f6f987cea6612d0a2ec
tinsleyfok <tslhuo@ucdavis.edu>
2017-02-07 14:59:11 -0800

    Speed

--- studio3/script.js
+++ studio3/script.js
@@ -49,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     function scrollHorizontally(e) {
         e = window.event || e;
         var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-        var scrollSpeed = 10;
+        var scrollSpeed = 20;
         document.documentElement.scrollLeft -= (delta * scrollSpeed);
         document.body.scrollLeft -= (delta * scrollSpeed);
         e.preventDefault();
