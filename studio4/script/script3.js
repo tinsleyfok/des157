f71c1dde8646a70d98386cb917e996404375b216
tinsleyfok <tslhuo@ucdavis.edu>
2017-02-14 10:20:03 -0800

    Change!

--- studio4/script/script3.js
+++ studio4/script/script3.js
@@ -3,25 +3,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
-    //menu
-var elements = document.getElementsByTagName('li');
-var tipTimer, tipId;
-
-for (var i = 0; i < elements.length; i++) {
-elements[i].addEventListener ('mouseover', function(event){
-  tipId = document.getElementById(this.id + "Tip");
-  tipTimer = setTimeout(showMsg, 500);
-});
-
-elements[i].addEventListener ('mouseout', function(){
-  clearTimeout(tipTimer);
-  tipId.style.opacity = 0;
-});
-}
-
-function showMsg(){
-  tipId.style.opacity = 1;
-}
     
     //page3
       var one=document.getElementById('one');
