a8989f330e32cfd41d56c5b40bbfbb78ee461e40
tinsleyfok <tslhuo@ucdavis.edu>
2017-03-24 05:11:21 -0700

    Ggg

--- final/js/project.js
+++ final/js/project.js
@@ -254,7 +254,8 @@ document.addEventListener("DOMContentLoaded", function () {
             objLoader.setMaterials(materials);
             objLoader.setPath('obj/obj_Grass/');
             objLoader.load('grass.obj', function (object) {
-                object.position.y = -10;
+                object.position.y = -50;
+                 object.position.x = 500;
                 scene.add(object);
             }, onProgress, onError);
         });
